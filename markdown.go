33f806ca62f50995f78ada7b899110861d6f163e
Kennet Mathisen <kennetam02@gmail.com>
2026-05-14 13:37:39 +0200

    setup for testing

---
+++ markdown.go
@@ -0,0 +1,7 @@
+package markdowntohtml
+
+import "errors"
+
+func Md2html(input string) (string, error) {
+	return "", errors.New("not yet implemented")
+}
---
+++ markdown_test.go
@@ -0,0 +1,23 @@
+package markdowntohtml_test
+
+import (
+	"fmt"
+	"testing"
+	"github.com/kegma1/markdown-to-HTML"
+)
+
+func TestH1(t *testing.T) {
+	msg := "Hallo verden"
+	
+	html, err := markdowntohtml.Md2html(fmt.Sprintf("# %s", msg))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if html != fmt.Sprintf("<h1>%s</h1>", msg) {
+		t.Log(html)
+		t.Fail()
+	}
+}
